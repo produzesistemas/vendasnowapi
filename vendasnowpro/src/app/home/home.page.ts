edfd43390c84dd434501140051be89fcc60cb513
paulo.sergio <paulo.sergio@consultores.reconcavo.org.br>
2022-07-15 15:09:19 -0300

    Ajustes na tela de login

--- vendasnowpro/src/app/home/home.page.ts
+++ vendasnowpro/src/app/home/home.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginUser } from '../_model/login-user-model';
+import { AuthenticationService } from '../_services/authentication.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public loginUser: LoginUser = new LoginUser();
+  form: FormGroup;
+  public submitted = false;
+  constructor(    private authenticationService: AuthenticationService,
+    private formBuilder: FormBuilder) {}
 
-  constructor() {}
+    get f() { return this.form.controls; }
+
+    ngOnInit() {
+      // if (this.authenticationService.getCurrentUser()) {
+      //     this.router.navigate(['partner-area']);
+      // }
+      this.form = this.formBuilder.group({
+          email: ['', Validators.required],
+          secret: ['', Validators.required]
+      });
+  }
+
+  onLogin() {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+  }
 
 }
